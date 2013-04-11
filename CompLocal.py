2eec584aa6b4870ca597427a9786f9d8c1d2b1bb
sddhrthrt <sddhrthrt@gmail.com>
2013-04-11 13:15:54 +0530

    CompLocal

---
+++ CompLocal.py
@@ -0,0 +1,20 @@
+class CompLocal:
+    def __init__(self):
+        self.totalfree=[]
+        return
+
+    def compute(self):
+        import UnixShell, string, StringIO
+        # do some real work here
+        result = UnixShell.getCommandOutput("df")
+        fd = StringIO.StringIO(result)
+        for line in fd.readlines():
+            if line[0] != 'F': # skip the first line
+                words = string.split(line) # extract fourth column
+                if len(words) == 6:
+                    self.totalfree = self.totalfree + [int(words[3])]
+                elif len(words) == 5:
+                    self.totalfree = self.totalfree + [int(words[2])]
+                else:
+                    print "\t Unexpected output from shell"
+        return self.totalfree
