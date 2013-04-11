be5cb20c2bd126b50b470b53c51711c7d43b5931
sddhrthrt <sddhrthrt@gmail.com>
2013-04-11 13:09:13 +0530

    added __str__ and __eq__

--- DiskAgent.py
+++ DiskAgent.py
@@ -46,4 +46,10 @@ class DiskAgent:
         except rsa.pkcs1.VerificationError as e:
             print "Cannot trust the code, cowardly exiting"
             raise e
+    
+    def __str__(self):
+        return str(self.__dict__)
 
+    def __eq__(self, other):
+        print "comparing"
+        return self.__dict__ == other.__dict__
